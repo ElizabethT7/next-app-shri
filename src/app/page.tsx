adc616c7248c54f42da303956e2692291e3acaf5
ElizabethT7 <tymofieievai@gmail.com>
2023-07-17 12:07:01 +0300

    feat: add first component (films)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+import React from 'react';
+import { Films } from '@/components/films/Films';
+
+
 export default function Home() {
-  return null;
+  return (
+    //react dom element 
+
+    /*React.createElement('span', {
+      children: '',
+    })*/
+    <>
+      <div>Hello students!</div>
+      <Films />
+    </>
+  );
 }
+
+    //react component
+    /*
+    class FirstComponent extends React.Component {
+      //...
+      render() {
+        //...
+      }
+      //...
+    }
+
+    function FirstComponent() {
+      //...
+      return ...
+    }
+
+    const FirstComponent = () => {
+      //...
+      return ReactElement || null;
+    }
+    */ 
---
+++ src/components/films/Films.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Films = () => {
+    /*return React.createElement('div', {
+    children: [
+      React.createElement('span', {children: 'Оно'}),
+      React.createElement('span', {children: 'Оно 2'})
+    ]
+  });*/
+  return (
+    <div>
+      <span>ОНО</span>
+      <span>ОНО 2</span>
+    </div>
+  );
+}
